c2b2ee449820a5fcdf50ed46a1b115f9f6a44b27
BinuxLiu <binuxliu@gmail.com>
2023-03-18 01:32:49 +0800

    fix some bugs

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -156,7 +156,7 @@ def __random_zoom(img, target_width, crop_width, method=Image.BICUBIC, factor=No
 def __scale_shortside(img, target_width, crop_width, method=Image.BICUBIC):
     ow, oh = img.size
     shortside = min(ow, oh)
-    if shortside >= target_width:
+    if shortside <= target_width:
         return img
     else:
         scale = target_width / shortside
